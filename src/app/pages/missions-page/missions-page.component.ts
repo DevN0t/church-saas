069a72ec38de2c3c1905f1ea4090d22b536b3dac
cayna <cayna@agapesistemas.com.br>
2025-02-11 02:22:15 -0300

    feat: Mission Page

---
+++ src/app/pages/missions-page/missions-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-missions-page',
+  imports: [],
+  templateUrl: './missions-page.component.html',
+  styleUrl: './missions-page.component.css',
+  standalone: true
+})
+export class MissionsPageComponent {
+
+}
