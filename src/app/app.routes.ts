c34a559278d47feddf6b09f606b53d830f299e43
cayna <cayna@agapesistemas.com.br>
2025-02-11 02:19:30 -0300

    feat: AuthGuard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,30 @@
 import { Routes } from '@angular/router';
 import {MainPageComponent} from './pages/main-page/main-page.component';
+import {LoginComponent} from './pages/login/login.component';
+import {HomePageComponent} from './pages/home-page/home-page.component';
+import {BannerPageComponent} from './pages/banner-page/banner-page.component';
+import {MissionsPageComponent} from './pages/missions-page/missions-page.component';
+import {MainDashboardPageComponent} from './pages/main-dashboard-page/main-dashboard-page.component';
+import {AuthGuard} from './infra/authguard';
 
 export const routes: Routes = [
   {
     path: '', component: MainPageComponent
+  },
+  {
+    path: 'login', component: LoginComponent
+  },
+  {
+    path: 'dashboard', component: HomePageComponent, canActivate: [AuthGuard], children: [
+      {
+        path: 'banner', component: BannerPageComponent
+      },
+      {
+        path: 'missions', component: MissionsPageComponent
+      },
+      {
+        path: '', component: MainDashboardPageComponent
+      }
+    ]
   }
 ];
